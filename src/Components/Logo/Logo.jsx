29480ab24b2dfcad609273bcdbb07264992d4737
BrianMagnan <BrianNMagnan@gmail.com>
2025-08-04 14:58:19 -0400

    Fix logo import path for production build

--- src/Components/Logo/Logo.jsx
+++ src/Components/Logo/Logo.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
+import logo from "../../assets/Vary-Suite-Logo.svg";
 import "./Logo.css";
 
 function Logo({ onClose }) {
   return (
     <Link to="/" onClick={onClose}>
-      <img className="logo" src="src/assets/Vary-Suite-Logo.svg" alt="logo" />
+      <img className="logo" src={logo} alt="logo" />
     </Link>
   );
 }
